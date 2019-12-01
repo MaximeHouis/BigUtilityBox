3612c80a3a73a4ebd057061cea548fe147688620
Maxime Houis <maxime.houis@epitech.eu>
2019-12-01 23:29:23 +0100

    Add TypeDisplayer

---
+++ BUB/include/BUB/TypeDisplayer.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** BigUtilityBox
+** File description:
+** TypeDisplayer.hpp
+*/
+
+#pragma once
+
+namespace bub {
+
+    /**
+     * This template can detect the type of a variable at compilation time.
+     * It's useful when you are having doubts about auto or decltype deduction.
+     *
+     * Inspired from Scott Meyer's Effective Modern C++.
+     */
+    template<typename>
+    class TypeDisplayer;
+
+    // In order to display the type deduction at compile time, we need to force a compilation error.
+
+}
